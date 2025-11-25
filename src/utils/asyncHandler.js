fef75fc05b6e287dfec7a64c1b6d301cc9291ea0
Manish <manishgkapse11@gmail.com>
2025-11-25 22:35:30 +0530

    add user and video model

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asynchHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+export { asynchHandler };
+
+// const asynchHandler = (fn) => async(req,res,next)=> {
+//   try {
+//     await fn(req,res,next);
+//   }catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message || "Internal Server Error",
+//     });
+//   }
+// }
